5392721f3736301807e970d99ac85cb74062f61a
A <LALEH@VAHIDs-MacBook-Pro.local>
2017-10-19 21:57:30 +0330

    2th commit

--- vahidWeb/models.py
+++ vahidWeb/models.py
@@ -1,3 +1,13 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
 from django.db import models
 
+
+
 # Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
