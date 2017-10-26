390a1814b676226ac9c5f2371cf4bb3f51d422b1
A <LALEH@VAHIDs-MacBook-Pro.local>
2017-10-27 00:12:51 +0330

    well well well

--- vahidWeb/admin.py
+++ vahidWeb/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Expense
+from .models import Expense , Income
 # Register your models here.
 admin.site.register(Expense)
+admin.site.register(Income)
--- vahidWeb/models.py
+++ vahidWeb/models.py
@@ -4,10 +4,22 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-
 # Create your models here.
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
